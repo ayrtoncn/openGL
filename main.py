e5f6168a21e225817c188b218cec68630658ff86
xRebellion <32483348+xRebellion@users.noreply.github.com>
2017-10-27 01:23:25 +0700

    updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-from OpenGLContext import testingcontext
-BaseContext = testingcontext.getInteractive()
+# 2D GRAPHICS
+# WITH OPENGL
+
+# IMPORT PYGAME OPENGL NUMPY (for matrix)
+import pygame
+from pygame.locals import *
+
 from OpenGL.GL import *
+from OpenGL.GLU import *
+
+import numpy as np
+from numpy import matrix
+
+# END OF IMPORT
+
+# FUNCTIONS
+
+def getPoints():
+	return input().split(",")
+
+def main():
+	N = int(input()) # INPUT N
+	PList = [] # DEFINE EMPTY LIST
+	for i in range(N): # GET POINTS
+		X, Y = getPoints()
+		PList.append([int(X),int(Y)]) 
+
+	Points = np.array(PList)
+	
+	
+main()
