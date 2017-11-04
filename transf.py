2a45b4b9926e0ec395e91d3450cd9ef72fb016f3
xRebellion <xRebellion@users.noreply.github.com>
2017-11-04 14:09:50 +0700

    added multiple

--- transf.py
+++ transf.py
@@ -10,6 +10,11 @@ import time
 blocking=False
 rotasi = False
 pertama = True
+isMultiple = False
+multAnimateFinish = True
+NMult = 0
+InputList = []
+iMult = 0
 
 
 
@@ -51,6 +56,29 @@ def animasi():
 	time.sleep(0.00001)
 	if(i==100):
 		blocking=False
+		rotasi=False
+		
+def animasiMult():
+	global multAnimateFinish
+	global i
+	global rotasi
+	j=0
+	if(not(rotasi)):
+		for points in M:
+			points[0,0]=points[0,0]+Mtemp[j][0]
+			points[0,1]=points[0,1]+Mtemp[j][1]
+			j=j+1
+	else:
+		for points in M:
+			X1=points[0,0]
+			Y1=points[0,1]
+			points[0,0]=(cos(sdtanimasi)*(X1-Xrotasi)-(sin(sdtanimasi))*(Y1-Yrotasi))+Xrotasi
+			points[0,1]=(sin(sdtanimasi)*(X1-Xrotasi)+(cos(sdtanimasi))*(Y1-Yrotasi))+Yrotasi
+	i=i+1
+	
+	time.sleep(0.00001)
+	if(i==100):
+		multAnimateFinish=True
 		rotasi=False
 
 def translate(X,Y):
@@ -130,14 +158,19 @@ def stretch(sumbu,K):
 	blocking=True	
 	
 def transform():
-	if(not(blocking)):
+	global isMultiple
+	global multAnimateFinish
+	global iMult
+	global NMult
+	global InputList
+	if(not(blocking) and not(isMultiple)):
 		if(not(pertama)):
 			n=input()
 			inpTransfType=n.split(' ')
 			if inpTransfType[0] == "translate" :
 				X=int(inpTransfType[1])
 				Y=int(inpTransfType[2])
-				translate(X,Y)			
+				translate(X,Y)	
 			elif inpTransfType[0] == "dilate" :
 				dil=float(inpTransfType[1])
 				dilate(dil)			
@@ -187,11 +220,15 @@ def transform():
 				stretch(sumbu,K)
 			
 			elif inpTransfType[0] == 'multiple':
+				multAnimateFinish = True
+				iMult = 0
 				InputList = []
+				isMultiple = True
 				for i in range(int(inpTransfType[1])):
 					multInput = input().split(' ')
 					InputList.append(multInput)
-				print(InputList)
+				NMult = int(inpTransfType[1])
+				
 			elif inpTransfType[0]=='reset':
 				j=0
 				for points in M:
@@ -201,9 +238,71 @@ def transform():
 			elif inpTransfType[0]=='exit':
 				exit()
 			
-	if(blocking):
+	if(blocking and not(isMultiple)):
 		animasi()
-	#glutSwapBuffers()
+	if(isMultiple):
+		if(multAnimateFinish):
+			transformList(InputList[iMult])
+			iMult += 1
+			multAnimateFinish = False
+		else:
+			animasiMult()
+		if(iMult == NMult):
+			isMultiple = False
 	return M
 	
-
+def transformList(inputList):
+	
+	inputType=inputList[0]
+	if inputType == "translate" :
+		X=int(inputList[1])
+		Y=int(inputList[2])
+		translate(X,Y)			
+	elif inputType == "dilate" :
+		dil=float(inputList[1])
+		dilate(dil)			
+	elif inputType == "rotate" :
+		sdt=radians(float(inputList[1]))
+		X=int(inputList[2])
+		Y=int(inputList[3])
+		rotate(sdt,X,Y)		
+	elif inputType == "reflect" :
+		param=inputList[1]
+		temp=param.split(',')
+		if(len(temp)==1):
+			if(param=='y=x'):
+				for points in M:
+					temp=points[0,0]
+					points[0,0]=-points[0,1]
+					points[0,1]=-temp
+			elif(param=='y=-x'):
+				for points in M:
+					temp=points[0,0]
+					points[0,0]=points[0,1]
+					points[0,1]=temp
+			elif(param=='x'):
+				for points in M:
+					points[0,1]=-points[0,1]
+			elif(param=='y'):
+				for points in M:
+					points[0,0]=-points[0,0]
+		else:
+			X=float(temp[0][1:])
+			Y=float(temp[1][:len(temp[1])-1])
+			sdt=radians(180)
+			for points in M:
+				X1=points[0,0]
+				Y1=points[0,1]
+				points[0,0]=(cos(sdt)*(X1-X)-(sin(sdt))*(Y1-Y))+X
+				points[0,1]=(sin(sdt)*(X1-X)+(cos(sdt))*(Y1-Y))+Y
+		
+	elif inputType == "sheer" :
+		sumbu=inputList[1]
+		K=float(inputList[2])
+		sheer(sumbu,K)
+		
+	elif inputType == "stretch" :
+		sumbu=inputList[1]
+		K=float(inputList[2])
+		stretch(sumbu,K)
+	
